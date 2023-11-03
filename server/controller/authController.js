4fd4859878afc521db05c9cb850edd7d7b10eb83
Waseemarshad97 <wakk97@gmail.com>
2023-11-03 01:45:37 +0000

    login & listusers apis added

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -31,7 +31,58 @@ exports.register = async (req, res, next) => {
     });
   } catch (error) {
     res.status(500).json({
-      error: `${error}`,
+      error: `${error.message}`,
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  console.log("hit");
+
+  // validate email & password - later validation can be moved to middleware
+  if (!email || !password) {
+    return res.status(500).json({
+      error: "Email and password are required",
+    });
+  }
+
+  try {
+    // Check if the user exists
+    const user = await User.findOne({ email: email });
+    if (user) {
+      return res.status(200).json({
+        data: user,
+      });
+    } else {
+      return res.status(420).json({
+        error: "user doesnt exist",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: `${error.message}`,
+    });
+  }
+};
+
+exports.listUsers = async (req, res, next) => {
+  try {
+    // Retrieve all users
+    const users = await User.find();
+
+    if (users.length > 0) {
+      return res.status(200).json({
+        data: users,
+      });
+    } else {
+      return res.status(404).json({
+        error: "No users found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
     });
   }
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { register } = require("../controller/authController");
+const { register, login, listUsers } = require("../controller/authController");
 
 router.post("/register", register);
+router.post("/login", login);
+router.post("/list", listUsers);
+
 module.exports = router;
